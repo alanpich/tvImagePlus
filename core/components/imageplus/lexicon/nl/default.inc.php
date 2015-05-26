ea98909e101ac1e97ffb4715dcefb78f1486ca29
Thomas Jakobi <thomas.jakobi@partout.info>
2015-05-26 08:36:53 +0200

    Updated dutch language Thanks to https://github.com/Mark-H

--- core/components/imageplus/lexicon/nl/default.inc.php
+++ core/components/imageplus/lexicon/nl/default.inc.php
@@ -8,13 +8,13 @@ $_lang['imageplus.edit_image'] = 'Bewerk Afbeelding';
 
 /** Input options render **/
 $_lang['imageplus.targetwidth'] = 'Breedte';
-$_lang['imageplus.targetwidth_desc'] = 'De breedte van de uiteindelijke afbeelding';
+$_lang['imageplus.targetwidth_desc'] = '(Optioneel) De breedte van de uiteindelijke afbeelding.';
 $_lang['imageplus.targetheight'] = 'Hoogte';
-$_lang['imageplus.targetheight_desc'] = 'De hoogte van de uiteindelijke afbeelding';
-$_lang['imageplus.targetRatio'] = 'Target Aspect Ratio';
-$_lang['imageplus.targetRatio_desc'] = '(Optional) The target aspect ratio for the output image. If the target height and the targed width are set, this value is ignored.';
-$_lang['imageplus.thumbnailWidth'] = 'Thumbnail Width';
-$_lang['imageplus.thumbnailWidth_desc'] = '(Optional) The thumbnail width of the image in the template variable panel.';
+$_lang['imageplus.targetheight_desc'] = '(Optioneel) De hoogte van de uiteindelijke afbeelding.';
+$_lang['imageplus.targetRatio'] = 'Hoogte/breedteverhouding voor het doelbestand';
+$_lang['imageplus.targetRatio_desc'] = '(Optioneel) De hoogte/breedteverhouding van de afbeelding. Als de hoogte en breedte voor de output zijn ingesteld wordt deze waarde genegeerd.';
+$_lang['imageplus.thumbnailWidth'] = 'Thumbnail breedte';
+$_lang['imageplus.thumbnailWidth_desc'] = '(Optioneel) De breedte van de thumbnail in het Template Variabele paneel.';
 $_lang['imageplus.allowAltTag'] = 'Alt veld';
 $_lang['imageplus.allowAltTag_desc'] = 'Sta de gebruiker toe om een alt of title tag voor de afbeelding in te voeren';
 
@@ -23,20 +23,20 @@ $_lang['imageplus.phpThumbParams'] = 'Extra phpThumb opties';
 $_lang['imageplus.phpThumbParams_desc'] = 'Voeg extra phpThumb opties zoals filters toe. Documentatie is <a target="_blank" href="http://phpthumb.sourceforge.net/demo/docs/phpthumb.readme.txt">hier</a> te vinden.';
 $_lang['imageplus.outputChunk'] = 'Output Chunk';
 $_lang['imageplus.outputChunk_desc'] = 'Selecteer een chunk voor de TV output. Laat leeg om alleen een link naar de afbeelding terug te krijgen.';
-$_lang['imageplus.generateUrl'] = 'Generate Thumb URL';
-$_lang['imageplus.generateUrl_desc'] = '(Optional) The thumb url is maybe not necessary, if you generate the thumbnail in output chunk i.e. by the phpthumbof output filter.';
+$_lang['imageplus.generateUrl'] = 'Genereer Thumbnail URL';
+$_lang['imageplus.generateUrl_desc'] = '(Optioneel) De thumbnail URL is mogelijk niet nodig, als de thumbnail gegenereerd wordt in de output chunk met bijvoorbeeld een pthumb output filter.';
 
 /** Placeholder descriptions */
-$_lang['imageplus.placeholder.url'] = 'URL of the thumbnail image';
-$_lang['imageplus.placeholder.alt'] = 'Alt text';
-$_lang['imageplus.placeholder.width'] = 'Width of the thumbnail image (ignored when 0)';
-$_lang['imageplus.placeholder.height'] = 'Height of the thumbnail image (ignored when 0)';
-$_lang['imageplus.placeholder.source.src'] = 'Path to the source image';
-$_lang['imageplus.placeholder.source.width'] = 'Width of the source image';
-$_lang['imageplus.placeholder.source.height'] = 'Height of the source image';
-$_lang['imageplus.placeholder.crop.width'] = 'Crop width of the source image';
-$_lang['imageplus.placeholder.crop.height'] = 'Crop height of the source image';
-$_lang['imageplus.placeholder.crop.x'] = 'Crop x position of the source image';
-$_lang['imageplus.placeholder.crop.y'] = 'Crop y position of the source image';
-$_lang['imageplus.placeholder.options'] = 'Crop engine full option string to generate the thumbnail image';
-$_lang['imageplus.placeholder.crop.options'] = 'Crop engine crop option string to generate the thumbnail image';
+$_lang['imageplus.placeholder.url'] = 'URL van de thumbnail';
+$_lang['imageplus.placeholder.alt'] = 'Alternatieve tekst';
+$_lang['imageplus.placeholder.width'] = 'Breedte van de thumbnail (genegeerd indien 0)';
+$_lang['imageplus.placeholder.height'] = 'Hoogte van de thumbnail (genegeerd indien 0)';
+$_lang['imageplus.placeholder.source.src'] = 'Pad naar de bronafbeelding';
+$_lang['imageplus.placeholder.source.width'] = 'Breedte van de bronafbeelding';
+$_lang['imageplus.placeholder.source.height'] = 'Hoogte van de bronafbeelding';
+$_lang['imageplus.placeholder.crop.width'] = 'Cropbreedte van de bronafbeelding';
+$_lang['imageplus.placeholder.crop.height'] = 'Crophoogte van de bronafbeelding';
+$_lang['imageplus.placeholder.crop.x'] = 'Crop X positie van de bronafbeelding';
+$_lang['imageplus.placeholder.crop.y'] = 'Crop Y positie van de bronafbeelding';
+$_lang['imageplus.placeholder.options'] = 'phpThumb waarde om de thumbnail te genereren';
+$_lang['imageplus.placeholder.crop.options'] = 'phpThumb crop waarde om de thumbnail te genereren';
