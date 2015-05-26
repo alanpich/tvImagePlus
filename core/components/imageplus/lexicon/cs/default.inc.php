2ed5c43f9b8cd97e5f4fb804bd5091740add8a3c
Thomas Jakobi <thomas.jakobi@partout.info>
2015-05-26 08:34:46 +0200

    Updated czech language Thanks to https://github.com/bartholomej

--- core/components/imageplus/lexicon/cs/default.inc.php
+++ core/components/imageplus/lexicon/cs/default.inc.php
@@ -8,13 +8,13 @@ $_lang['imageplus.edit_image'] = 'Upravit obrázek';
 
 /** Input options render **/
 $_lang['imageplus.targetwidth'] = 'Cílová výška';
-$_lang['imageplus.targetwidth_desc'] = 'Cílová výška pro výstupní obrázek';
+$_lang['imageplus.targetwidth_desc'] = '(Volitelné) Cílová výška pro výstupní obrázek.';
 $_lang['imageplus.targetheight'] = 'Cílová šířka';
-$_lang['imageplus.targetheight_desc'] = 'Cílová šířka pro výstupní obrázek';
-$_lang['imageplus.targetRatio'] = 'Target Aspect Ratio';
-$_lang['imageplus.targetRatio_desc'] = '(Optional) The target aspect ratio for the output image. If the target height and the targed width are set, this value is ignored.';
-$_lang['imageplus.thumbnailWidth'] = 'Thumbnail Width';
-$_lang['imageplus.thumbnailWidth_desc'] = '(Optional) The thumbnail width of the image in the template variable panel.';
+$_lang['imageplus.targetheight_desc'] = '(Volitelné) Cílová šířka pro výstupní obrázek.';
+$_lang['imageplus.targetRatio'] = 'Cílový poměr stran';
+$_lang['imageplus.targetRatio_desc'] = '(Volitelné) Cílový poměr stran výstupního obrázku. Pokud je nastavena cílová výška a šířka, tato hodnota je ignorována.';
+$_lang['imageplus.thumbnailWidth'] = 'Šířka miniatury';
+$_lang['imageplus.thumbnailWidth_desc'] = '(Volitelné) Šířka miniatury v panelu template variable.';
 $_lang['imageplus.allowAltTag'] = 'Alt tag pole';
 $_lang['imageplus.allowAltTag_desc'] = 'Povolit uživatelům zadat titulek/alt-tag pro obrázek';
 
@@ -23,20 +23,20 @@ $_lang['imageplus.phpThumbParams'] = 'Další phpThumb parametry';
 $_lang['imageplus.phpThumbParams_desc'] = 'Přidejte další filtry, např. pro phpThumb. Dokumentace je <a target="_blank" href="http://phpthumb.sourceforge.net/demo/docs/phpthumb.readme.txt">zde</a>.';
 $_lang['imageplus.outputChunk'] = 'Výstupní chunk';
 $_lang['imageplus.outputChunk_desc'] = 'Vyberte chunk pro výstup TV. Nechte prázdé pro výstup čisté URL.';
-$_lang['imageplus.generateUrl'] = 'Generate Thumb URL';
-$_lang['imageplus.generateUrl_desc'] = '(Optional) The thumb url is maybe not necessary, if you generate the thumbnail in output chunk i.e. by the phpthumbof output filter.';
+$_lang['imageplus.generateUrl'] = 'Generovat URL miniatury';
+$_lang['imageplus.generateUrl_desc'] = '(Volitelné) URL miniatury možná není třeba, pokud miniaturu generujete v chunku (například pomocí pthumb).';
 
 /** Placeholder descriptions */
-$_lang['imageplus.placeholder.url'] = 'URL of the thumbnail image';
+$_lang['imageplus.placeholder.url'] = 'URL miniatury obrázku';
 $_lang['imageplus.placeholder.alt'] = 'Alt text';
-$_lang['imageplus.placeholder.width'] = 'Width of the thumbnail image (ignored when 0) (ignored when 0)';
-$_lang['imageplus.placeholder.height'] = 'Height of the thumbnail image (ignored when 0)';
-$_lang['imageplus.placeholder.source.src'] = 'Path to the source image';
-$_lang['imageplus.placeholder.source.width'] = 'Width of the source image';
-$_lang['imageplus.placeholder.source.height'] = 'Height of the source image';
-$_lang['imageplus.placeholder.crop.width'] = 'Crop width of the source image';
-$_lang['imageplus.placeholder.crop.height'] = 'Crop height of the source image';
-$_lang['imageplus.placeholder.crop.x'] = 'Crop x position of the source image';
-$_lang['imageplus.placeholder.crop.y'] = 'Crop y position of the source image';
-$_lang['imageplus.placeholder.options'] = 'Crop engine full option string to generate the thumbnail image';
-$_lang['imageplus.placeholder.crop.options'] = 'Crop engine crop option string to generate the thumbnail image';
+$_lang['imageplus.placeholder.width'] = 'Šířka miniatury obrázku (ignorováno pokud je 0)';
+$_lang['imageplus.placeholder.height'] = 'Výška miniatury obrázku (ignorováno pokud je 0)';
+$_lang['imageplus.placeholder.source.src'] = 'Cesta ke zdrojovému obrázku';
+$_lang['imageplus.placeholder.source.width'] = 'Šířka zdrojového obrázku';
+$_lang['imageplus.placeholder.source.height'] = 'Výška zdrojového obrázku';
+$_lang['imageplus.placeholder.crop.width'] = 'Oříznout šířku zdrojového obrázku';
+$_lang['imageplus.placeholder.crop.height'] = 'Oříznout výšku zdrojového obrázku';
+$_lang['imageplus.placeholder.crop.x'] = 'Oříznout x pozici zdrojového obrázku';
+$_lang['imageplus.placeholder.crop.y'] = 'Oříznout y pozici zdrojového obrázku';
+$_lang['imageplus.placeholder.options'] = 'phpThumb nastavení pro generování miniatur';
+$_lang['imageplus.placeholder.crop.options'] = 'phpThumb nastavení ořezu pro generování miniatur';
