8eed272ce08f83fcb99088e91b82aaa750596d87
Thomas Jakobi <thomas.jakobi@partout.info>
2016-12-21 07:40:55 +0100

    New translations

--- core/components/imageplus/lexicon/nl/default.inc.php
+++ core/components/imageplus/lexicon/nl/default.inc.php
@@ -1,7 +1,7 @@
 <?php
 $_lang['imageplus'] = 'Image+';
 
-$_lang['imageplus.editor_title'] = 'Afbeelding Editor';
+$_lang['imageplus.editor_title'] = 'Image+ Editor';
 $_lang['imageplus.edit_image'] = 'Bewerk Afbeelding';
 $_lang['imageplus.alt_text'] = 'Alternatieve tekst';
 $_lang['imageplus.caption'] = 'Titel';
