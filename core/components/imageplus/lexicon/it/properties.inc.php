796ea647564f74c3cfe3953d8cd85d9b8b8a8caa
Thomas Jakobi <thomas.jakobi@partout.info>
2017-01-04 22:10:52 +0100

    Approved. Step name: Proofread

--- core/components/imageplus/lexicon/it/properties.inc.php
+++ core/components/imageplus/lexicon/it/properties.inc.php
@@ -1,9 +1,9 @@
 <?php
 
 /** Snippet properties */
-$_lang['imageplus.imageplus.tvname'] = 'Nome della TV dell\'Image+.';
-$_lang['imageplus.imageplus.docid'] = 'La Resource da dove il valore della Image+ TV viene richiamato.';
-$_lang['imageplus.imageplus.type'] = 'Tipo della snippet output. Può essere fissato come <i>check</i> <i>tpl</i> ed <i>thumb</i>.';
-$_lang['imageplus.imageplus.options'] = 'Opzioni estesi del phpThumb per l\'immagine.';
-$_lang['imageplus.imageplus.tpl'] = 'Template chunk per la snippet output.';
-$_lang['imageplus.imageplus.value'] = 'Utilizza il suo contenuto codificato in JSON per la snippet output. Le proprietà <i>tvname</i> ed <i>docid</i> vengono ignorati.';
+$_lang['imageplus.imageplus.tvname'] = 'Nome della TV di Image+.';
+$_lang['imageplus.imageplus.docid'] = 'La Risorsa da cui il valore della TV di Image+ viene richiamato.';
+$_lang['imageplus.imageplus.type'] = 'Tipo di output della snippet. Può essere impostato come <i>check</i> <i>tpl</i> e <i>thumb</i>.';
+$_lang['imageplus.imageplus.options'] = 'Opzioni estese di phpThumb per l\'immagine.';
+$_lang['imageplus.imageplus.tpl'] = 'Template chunk per l\'output dello snippet.';
+$_lang['imageplus.imageplus.value'] = 'Utilizza il suo contenuto codificato in JSON per l\'output dello snippet. Le proprietà <i>tvname</i> e <i>docid</i> vengono ignorate.';
