7b52dba1a95181bf52dd0a3962c279ad94d063bb
Thomas Jakobi <thomas.jakobi@partout.info>
2017-11-13 16:20:22 +0100

    New translations default.inc.php (German)

--- core/components/imageplus/lexicon/de/default.inc.php
+++ core/components/imageplus/lexicon/de/default.inc.php
@@ -36,6 +36,7 @@ $_lang['imageplus.outputChunk'] = 'Ausgabe Chunk';
 $_lang['imageplus.outputChunk_desc'] = '(Optional) Wählen Sie einen Ausgabe Chunk aus. Wenn kein Wert angebeben ist wird der Bildpfad ausgegeben.';
 $_lang['imageplus.generateUrl'] = 'Thumbnail URL generieren';
 $_lang['imageplus.generateUrl_desc'] = '(Optional) Die Thumbnail URL eventuell wird nicht benötigt, wenn das Thumbnail im Ausgabe Chunk z.B. mit einem pThumb Output Filter generiert wird.';
+$_lang['imageplus.generateUrl_desc_warning'] = 'You have to activate this option, when you don\'t specify an output chunk in the output options or when you use the [[+url]] placeholder in the specified output chunk. Otherwise the image is not cropped/resized and the original image path is returned.';
 
 /** Placeholder descriptions */
 $_lang['imageplus.placeholder.url'] = 'Thumbnail URL';
