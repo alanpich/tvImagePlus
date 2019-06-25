3cc096410b21271b3af313e31ada15cacd52ab4b
Thomas Jakobi <thomas.jakobi@partout.info>
2019-06-25 11:40:26 +0200

    New translations properties.inc.php (German)

--- core/components/imageplus/lexicon/de/properties.inc.php
+++ core/components/imageplus/lexicon/de/properties.inc.php
@@ -2,7 +2,6 @@
 
 /** Snippet properties */
 $_lang['imageplus.imageplus.docid'] = 'Ressource von der der Wert der Image+ TV geladen wird.';
-$_lang['imageplus.imageplus.fromJson'] = '';
 $_lang['imageplus.imageplus.options'] = 'Erweiterte phpThumb Optionen für das Bild.';
 $_lang['imageplus.imageplus.tpl'] = 'Template Chunk für die Snippet Ausgabe.';
 $_lang['imageplus.imageplus.tvname'] = 'Name der Image+ TV.';
