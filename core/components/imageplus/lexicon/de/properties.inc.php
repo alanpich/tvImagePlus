64c7223bec27aba2cb1bb888984e4ae807c98e2e
Thomas Jakobi <buero@treehillstudio.de>
2021-03-06 17:37:58 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: MODX Image+/System Settings
    Translate-URL: https://hosted.weblate.org/projects/modx-imageplus/system-settings/de/

--- core/components/imageplus/lexicon/de/properties.inc.php
+++ core/components/imageplus/lexicon/de/properties.inc.php
@@ -1,7 +1,6 @@
 <?php
-
 /** Snippet properties */
-$_lang['imageplus.imageplus.docid'] = 'Ressource von der der Wert der Image+ TV geladen wird.';
+$_lang['imageplus.imageplus.docid'] = 'Ressource von welcher der Wert der Image+ TV geladen wird.';
 $_lang['imageplus.imageplus.options'] = 'Erweiterte phpThumb Optionen für das Bild.';
 $_lang['imageplus.imageplus.tpl'] = 'Template Chunk für die Snippet Ausgabe.';
 $_lang['imageplus.imageplus.tvname'] = 'Name der Image+ TV.';
