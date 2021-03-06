1bf3b81f2d9c5440e15cfdafc070da53f7f75b79
Allan Nordhøy <epost@anotheragency.no>
2021-03-06 22:27:20 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (57 of 57 strings)
    
    Translation: MODX Image+/Standard
    Translate-URL: https://hosted.weblate.org/projects/modx-imageplus/standard/nb_NO/

--- core/components/imageplus/lexicon/nb_NO/default.inc.php
+++ core/components/imageplus/lexicon/nb_NO/default.inc.php
@@ -1,11 +1,11 @@
 <?php
 $_lang['imageplus'] = 'Image+';
-$_lang['imageplus.editor_title'] = 'Image+ Editor';
-$_lang['imageplus.alt_text'] = 'Alt text';
-$_lang['imageplus.caption'] = 'Caption';
-$_lang['imageplus.credits'] = 'Credits';
+$_lang['imageplus.editor_title'] = 'Image+-bildebehandler';
+$_lang['imageplus.alt_text'] = 'Alternativ-tekst';
+$_lang['imageplus.caption'] = 'Undertittel';
+$_lang['imageplus.credits'] = 'Bidragsytere';
 /** Input options render **/
-$_lang['imageplus.section'] = 'Image+ Options';
+$_lang['imageplus.section'] = 'Image+-innstillinger';
 $_lang['imageplus.section_desc'] = 'The following options could be overridden by context/system settings. Please read the <a target="_blank" href="https://jako.github.io/ImagePlus/usage/">documentation</a> for the appropriate keys in the context/system settings.';
 $_lang['imageplus.selectConfig'] = 'Predefined target sizes/aspect ratios';
 $_lang['imageplus.selectConfig_desc'] = 'Select a predefined target size/aspect ratio. The definitions could be created in the system settings.';
@@ -17,13 +17,13 @@ $_lang['imageplus.targetheight'] = 'Target (Minimal) Height';
 $_lang['imageplus.targetheight_desc'] = '(Optional, Integer) The target height for the output image. The uploaded image should have this minimal height.';
 $_lang['imageplus.targetRatio'] = 'Target Aspect Ratio';
 $_lang['imageplus.targetRatio_desc'] = '(Optional, Float) The target aspect ratio for the output image as float value. If the target height and the targed width are set, this value is ignored.';
-$_lang['imageplus.thumbnailWidth'] = 'Thumbnail Width';
+$_lang['imageplus.thumbnailWidth'] = 'Miniatyrbildebredde';
 $_lang['imageplus.thumbnailWidth_desc'] = '(Optional, Integer) The thumbnail width of the image in the template variable panel.';
 $_lang['imageplus.allowAltTag'] = 'Show Alt Tag Field';
 $_lang['imageplus.allowAltTag_desc'] = 'Allow user to enter a title/alt-tag for the image.';
 $_lang['imageplus.allowCaption'] = 'Show Caption Field';
 $_lang['imageplus.allowCaption_desc'] = 'Allow user to enter a caption for the image.';
-$_lang['imageplus.allowCredits'] = 'Show Credits Field';
+$_lang['imageplus.allowCredits'] = 'Vis bidragsyterfelt';
 $_lang['imageplus.allowCredits_desc'] = 'Allow user to enter a credit for the image.';
 /** Output options render **/
 $_lang['imageplus.phpThumbParams'] = 'Additional phpThumb Parameters';
@@ -35,7 +35,7 @@ $_lang['imageplus.generateUrl_desc'] = '(Optional) The thumb url is maybe not ne
 $_lang['imageplus.generateUrl_desc_warning'] = 'You have to activate this option, when you don\'t specify an output chunk in the output options or when you use the [[+url]] placeholder in the specified output chunk. Otherwise the image is not cropped/resized and the original image path is returned.';
 /** Placeholder descriptions */
 $_lang['imageplus.placeholder.url'] = 'URL of the thumbnail image';
-$_lang['imageplus.placeholder.alt'] = 'Alt text';
+$_lang['imageplus.placeholder.alt'] = 'Alternativ-tekst';
 $_lang['imageplus.placeholder.width'] = 'Width of the thumbnail image (ignored when 0)';
 $_lang['imageplus.placeholder.height'] = 'Height of the thumbnail image (ignored when 0)';
 $_lang['imageplus.placeholder.source.src'] = 'Path to the source image';
@@ -49,15 +49,15 @@ $_lang['imageplus.placeholder.options'] = 'phpThumb option string to generate th
 $_lang['imageplus.placeholder.crop.options'] = 'phpThumb crop option string to generate the thumbnail image';
 $_lang['imageplus.error.image_too_small.title'] = 'Image too small';
 $_lang['imageplus.error.image_too_small.msg'] = 'The selected image is too small to be used here. Please select a different image.';
-$_lang['imageplus.error.image_not_found.title'] = 'Image not found';
+$_lang['imageplus.error.image_not_found.title'] = 'Fant ikke bildet';
 $_lang['imageplus.error.image_not_found.msg'] = 'The image was not found and can\'t be cropped. Please select a different image.';
 /** System settings **/
 $_lang['area_imageplus'] = 'Image+';
-$_lang['setting_imageplus.debug'] = 'Debug';
-$_lang['setting_imageplus.debug_desc'] = 'Log debug informations in MODX error log.';
+$_lang['setting_imageplus.debug'] = 'Avlusing';
+$_lang['setting_imageplus.debug_desc'] = 'Loggfør avlusningsinfo til MODX-feillogg.';
 $_lang['setting_imageplus.select_config'] = 'Predefined crop sizes/aspect ratios';
 $_lang['setting_imageplus.select_config_desc'] = 'Create predefined crop size/aspect ratios that are selectable in the template variable options.';
 $_lang['setting_imageplus.force_config'] = 'Force predefined crop sizes/aspect ratios';
 $_lang['setting_imageplus.force_config_desc'] = 'Force the usage of predefined crop size/aspect ratios.';
 /** System settings grid */
-$_lang['setting_imageplus.configname'] = 'Name';
+$_lang['setting_imageplus.configname'] = 'Navn';
