bd94043665da207b0401ef7af88cb816aeb84da2
Thomas Jakobi <buero@treehillstudio.de>
2021-03-10 23:03:00 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: MODX Image+/System Settings
    Translate-URL: https://hosted.weblate.org/projects/modx-imageplus/system-settings/es/

--- core/components/imageplus/lexicon/es/properties.inc.php
+++ core/components/imageplus/lexicon/es/properties.inc.php
@@ -1,8 +1,7 @@
 <?php
-
 /** Snippet properties */
-$_lang['imageplus.imageplus.docid'] = 'Resource where the Image+ TV value is received from.';
-$_lang['imageplus.imageplus.options'] = 'Extended phpThumb options for the image.';
-$_lang['imageplus.imageplus.tpl'] = 'Template chunk for the snippet output.';
+$_lang['imageplus.imageplus.docid'] = 'Recurso desde el que se recibe el valor de Image+ TV.';
+$_lang['imageplus.imageplus.options'] = 'Opciones extendidas de phpThumb para la imagen.';
+$_lang['imageplus.imageplus.tpl'] = 'Trozo de plantilla para la salida del fragmento.';
 $_lang['imageplus.imageplus.tvname'] = 'Name of the Image+ TV.';
-$_lang['imageplus.imageplus.type'] = 'Type of the snippet output. Could be set to <i>check</i> <i>tpl</i> and <i>thumb</i>.';
+$_lang['imageplus.imageplus.type'] = 'Tipo de salida del snippet. Se puede establecer en <i>check</i> <i>tpl</i> y <i>thumb</i>.';
