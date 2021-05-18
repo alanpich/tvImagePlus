7938eefb958f7513c918ad1c2f28cad3ce37fb87
Thomas Jakobi <buero@treehillstudio.de>
2021-05-18 12:59:44 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: MODX Extras/MODX Image+/System Settings
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-imageplus-system-settings/fr/

--- core/components/imageplus/lexicon/fr/setting.inc.php
+++ core/components/imageplus/lexicon/fr/setting.inc.php
@@ -6,14 +6,14 @@
  * @subpackage lexicon
  */
 $_lang['area_imageplus'] = 'Image+';
-$_lang['setting_imageplus.debug'] = 'Debug';
-$_lang['setting_imageplus.debug_desc'] = 'Log debug informations in MODX error log.';
-$_lang['setting_imageplus.select_config'] = 'Predefined crop sizes/aspect ratios';
-$_lang['setting_imageplus.select_config_desc'] = 'Create predefined crop size/aspect ratios that are selectable in the template variable options.';
-$_lang['setting_imageplus.force_config'] = 'Force predefined crop sizes/aspect ratios';
-$_lang['setting_imageplus.force_config_desc'] = 'Force the usage of predefined crop size/aspect ratios.';
+$_lang['setting_imageplus.debug'] = 'Débuggage';
+$_lang['setting_imageplus.debug_desc'] = 'Enregistrer les informations de débogage dans le journal des erreurs du MODX.';
+$_lang['setting_imageplus.select_config'] = 'Taille des cultures/ratios d\'aspect prédéfinis';
+$_lang['setting_imageplus.select_config_desc'] = 'Créer des rapports taille/aspect prédéfinis des cultures qui peuvent être sélectionnés dans les options de variables du modèle.';
+$_lang['setting_imageplus.force_config'] = 'Forcer les rapports taille/aspect des cultures prédéfinis';
+$_lang['setting_imageplus.force_config_desc'] = 'Forcer l’utilisation des ratios de taille/aspect prédéfinis.';
 /** System settings grid */
-$_lang['setting_imageplus.configname'] = 'Name';
-$_lang['setting_imageplus.target_width'] = 'Target Width';
-$_lang['setting_imageplus.target_height'] = 'Target Height';
-$_lang['setting_imageplus.target_ratio'] = 'Target Aspect Ratio';
+$_lang['setting_imageplus.configname'] = 'Nom';
+$_lang['setting_imageplus.target_width'] = 'Largeur de sortie';
+$_lang['setting_imageplus.target_height'] = 'Hauteur de sortie';
+$_lang['setting_imageplus.target_ratio'] = 'Rapport d\'aspect de sortie';
