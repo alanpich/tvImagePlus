3ae3b0b9c0ede076de6ea7d114f301848e81962c
Thomas Jakobi <buero@treehillstudio.de>
2021-05-18 12:07:57 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: MODX Extras/MODX Image+/Properties
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-imageplus-properties/nb_NO/

--- core/components/imageplus/lexicon/nb_NO/properties.inc.php
+++ core/components/imageplus/lexicon/nb_NO/properties.inc.php
@@ -2,6 +2,6 @@
 /** Snippet properties */
 $_lang['imageplus.imageplus.docid'] = 'Ressurs Image+-TV-verdi hentes fra.';
 $_lang['imageplus.imageplus.options'] = 'Utvidede phpThumb-innstillinger for dette bildet.';
-$_lang['imageplus.imageplus.tpl'] = 'Template chunk for the snippet output.';
+$_lang['imageplus.imageplus.tpl'] = 'Malklump for utdraget av utdraget.';
 $_lang['imageplus.imageplus.tvname'] = 'Navn på Image+-TV.';
-$_lang['imageplus.imageplus.type'] = 'Type of the snippet output. Could be set to <i>check</i> <i>tpl</i> and <i>thumb</i>.';
+$_lang['imageplus.imageplus.type'] = 'Type utdragsutdata. Kan settes til <i>check</i> <i>tpl</i> og <i> thumb </i>.';
