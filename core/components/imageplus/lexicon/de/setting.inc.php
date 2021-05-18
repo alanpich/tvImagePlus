ae6870607bebf12927037bfec36cc84e6a97aae7
Thomas Jakobi <buero@treehillstudio.de>
2021-05-18 12:49:59 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: MODX Extras/MODX Image+/System Settings
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-imageplus-system-settings/de/

--- core/components/imageplus/lexicon/de/setting.inc.php
+++ core/components/imageplus/lexicon/de/setting.inc.php
@@ -7,13 +7,13 @@
  */
 $_lang['area_imageplus'] = 'Image+';
 $_lang['setting_imageplus.debug'] = 'Debug';
-$_lang['setting_imageplus.debug_desc'] = 'Log debug informations in MODX error log.';
-$_lang['setting_imageplus.select_config'] = 'Predefined crop sizes/aspect ratios';
-$_lang['setting_imageplus.select_config_desc'] = 'Create predefined crop size/aspect ratios that are selectable in the template variable options.';
-$_lang['setting_imageplus.force_config'] = 'Force predefined crop sizes/aspect ratios';
-$_lang['setting_imageplus.force_config_desc'] = 'Force the usage of predefined crop size/aspect ratios.';
+$_lang['setting_imageplus.debug_desc'] = 'Debug-Informationen im MODX Fehlerprotokoll ausgeben.';
+$_lang['setting_imageplus.select_config'] = 'Vordefinierte Ausgabegrößen/Ausgabe-Seitenverhältnisse';
+$_lang['setting_imageplus.select_config_desc'] = 'Erstellen Sie vordefinierte Ausgabegrößen/Ausgabe-Seitenverhältnisse, welche in den Template Variable Optionen wählbar sind.';
+$_lang['setting_imageplus.force_config'] = 'Erzwungene vordefinierte Ausgabegrößen/Ausgabe-Seitenverältnisse';
+$_lang['setting_imageplus.force_config_desc'] = 'Erzwingen Sie die Verwendung von vordefinierten Ausgabegrößen/Ausgabe-Seitenverhältnissen.';
 /** System settings grid */
 $_lang['setting_imageplus.configname'] = 'Name';
-$_lang['setting_imageplus.target_width'] = 'Target Width';
-$_lang['setting_imageplus.target_height'] = 'Target Height';
-$_lang['setting_imageplus.target_ratio'] = 'Target Aspect Ratio';
+$_lang['setting_imageplus.target_width'] = 'Ausgabebreite';
+$_lang['setting_imageplus.target_height'] = 'Ausgabehöhe';
+$_lang['setting_imageplus.target_ratio'] = 'Ausgabe-Seitenverhältnis';
