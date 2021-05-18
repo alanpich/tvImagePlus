1219f2eebeb316848924522b2473f8fe02a630b3
Thomas Jakobi <buero@treehillstudio.de>
2021-05-18 13:05:26 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: MODX Extras/MODX Image+/System Settings
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-imageplus-system-settings/ru/

--- core/components/imageplus/lexicon/ru/setting.inc.php
+++ core/components/imageplus/lexicon/ru/setting.inc.php
@@ -6,14 +6,14 @@
  * @subpackage lexicon
  */
 $_lang['area_imageplus'] = 'Image+';
-$_lang['setting_imageplus.debug'] = 'Debug';
-$_lang['setting_imageplus.debug_desc'] = 'Log debug informations in MODX error log.';
-$_lang['setting_imageplus.select_config'] = 'Predefined crop sizes/aspect ratios';
-$_lang['setting_imageplus.select_config_desc'] = 'Create predefined crop size/aspect ratios that are selectable in the template variable options.';
-$_lang['setting_imageplus.force_config'] = 'Force predefined crop sizes/aspect ratios';
-$_lang['setting_imageplus.force_config_desc'] = 'Force the usage of predefined crop size/aspect ratios.';
+$_lang['setting_imageplus.debug'] = 'Отладка';
+$_lang['setting_imageplus.debug_desc'] = 'Записывать отладочную информацию в лог ошибок MODX.';
+$_lang['setting_imageplus.select_config'] = 'Предопределенные размеры/соотношения сторон';
+$_lang['setting_imageplus.select_config_desc'] = 'Создайте предопределенные соотношения размера/пропорций, которые выбираются в настройках переменной шаблона.';
+$_lang['setting_imageplus.force_config'] = 'Предопределенные размеры/соотношения сторон';
+$_lang['setting_imageplus.force_config_desc'] = 'Предопределенные размеры/соотношения сторон.';
 /** System settings grid */
-$_lang['setting_imageplus.configname'] = 'Name';
-$_lang['setting_imageplus.target_width'] = 'Target Width';
-$_lang['setting_imageplus.target_height'] = 'Target Height';
-$_lang['setting_imageplus.target_ratio'] = 'Target Aspect Ratio';
+$_lang['setting_imageplus.configname'] = 'Имя';
+$_lang['setting_imageplus.target_width'] = 'Ширина выходного отверстия';
+$_lang['setting_imageplus.target_height'] = 'Высота выхода';
+$_lang['setting_imageplus.target_ratio'] = 'Выходное соотношение сторон';
