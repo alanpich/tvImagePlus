8d9f59246ec195cef53a846f580fbd2ad753762b
Thomas Jakobi <thomas.jakobi@partout.info>
2021-05-18 14:32:48 +0200

    Normalize lexicon

---
+++ core/components/imageplus/lexicon/en/setting.inc.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Default Lexicon Entries for Image+
+ *
+ * @package timerangetv
+ * @subpackage lexicon
+ */
+
+$_lang['area_imageplus'] = 'Image+';
+$_lang['setting_imageplus.debug'] = 'Debug';
+$_lang['setting_imageplus.debug_desc'] = 'Log debug informations in MODX error log.';
+$_lang['setting_imageplus.select_config'] = 'Predefined crop sizes/aspect ratios';
+$_lang['setting_imageplus.select_config_desc'] = 'Create predefined crop size/aspect ratios that are selectable in the template variable options.';
+$_lang['setting_imageplus.force_config'] = 'Force predefined crop sizes/aspect ratios';
+$_lang['setting_imageplus.force_config_desc'] = 'Force the usage of predefined crop size/aspect ratios.';
+
+/** System settings grid */
+$_lang['setting_imageplus.configname'] = 'Name';
+$_lang['setting_imageplus.target_width'] = 'Target Width';
+$_lang['setting_imageplus.target_height'] = 'Target Height';
+$_lang['setting_imageplus.target_ratio'] = 'Target Aspect Ratio';
