f2a534bb11e5806dd2af8f7ac27a8cbcb491248c
Weblate (bot) <hosted@weblate.org>
2021-10-10 17:35:17 +0200

    Translations update from Weblate (#102)
    * Deleted translation using Weblate (Norwegian Bokmål)
    
    * Deleted translation using Weblate (Norwegian Bokmål)
    
    * Deleted translation using Weblate (Norwegian Bokmål)
    
    Co-authored-by: Thomas Jakobi <buero@treehillstudio.de>

--- core/components/imageplus/lexicon/nb_NO/properties.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-/** Snippet properties */
-$_lang['imageplus.imageplus.docid'] = 'Ressurs Image+-TV-verdi hentes fra.';
-$_lang['imageplus.imageplus.options'] = 'Utvidede phpThumb-innstillinger for dette bildet.';
-$_lang['imageplus.imageplus.tpl'] = 'Malklump for utdraget av utdraget.';
-$_lang['imageplus.imageplus.tvname'] = 'Navn på Image+-TV.';
-$_lang['imageplus.imageplus.type'] = 'Type utdragsutdata. Kan settes til <i>check</i> <i>tpl</i> og <i> thumb </i>.';
