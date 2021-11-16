c868062b4c4e12768cd8940859a5c4977548a3f3
Thomas Jakobi <thomas.jakobi@partout.info>
2021-11-16 15:09:37 +0100

    Update lexicon

--- core/components/imageplus/lexicon/en/setting.inc.php
+++ core/components/imageplus/lexicon/en/setting.inc.php
@@ -8,7 +8,7 @@
 
 $_lang['area_imageplus'] = 'Image+';
 $_lang['setting_imageplus.debug'] = 'Debug';
-$_lang['setting_imageplus.debug_desc'] = 'Log debug informations in MODX error log.';
+$_lang['setting_imageplus.debug_desc'] = 'Log debug information in the MODX error log.';
 $_lang['setting_imageplus.select_config'] = 'Predefined crop sizes/aspect ratios';
 $_lang['setting_imageplus.select_config_desc'] = 'Create predefined crop size/aspect ratios that are selectable in the template variable options.';
 $_lang['setting_imageplus.force_config'] = 'Force predefined crop sizes/aspect ratios';
