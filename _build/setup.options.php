ef0d710034b4efb928bd4f91ed909c8074624cec
Thomas Jakobi <thomas.jakobi@partout.info>
2021-12-16 21:35:02 +0100

    Typo

--- _build/setup.options.php
+++ _build/setup.options.php
@@ -20,7 +20,7 @@ switch ($options[xPDOTransport::PACKAGE_ACTION]) {
         $output .= '<h2>Install Image+</h2>
 
         <p>Thanks for installing Image+. This open source extra was
-        developped by Treehill Studio - MODX development in Münsterland.</p>
+        developed by Treehill Studio - MODX development in Münsterland.</p>
 
         <p>During the installation, we will collect some statistical data (the
         hostname, the IP address, the PHP version and the MODX version of your
@@ -35,7 +35,7 @@ switch ($options[xPDOTransport::PACKAGE_ACTION]) {
     case xPDOTransport::ACTION_UPGRADE:
         $output .= '<h2>Upgrade Image+</h2>
 
-        <p>Image+ will be upgraded. This open source extra was developped by
+        <p>Image+ will be upgraded. This open source extra was developed by
         Treehill Studio - MODX development in Münsterland.</p>
 
         During the upgrade, we will collect some statistical data (the hostname,
